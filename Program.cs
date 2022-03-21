6cd568f4157f6826183b2ba8b94e335434a690be
JoaoPedroCordoba <jp.8pedro86@gmail.com.br>
2022-03-21 01:07:38 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string frasedigitada, frasememetomervoso;
+
+Console.WriteLine("Usuario,digite por favor oque você acha desse programa:");
+frasedigitada= Console.ReadLine()!;
+
+frasememetomervoso = $"{frasedigitada.Remove(10)}... NÃO, PERA.\nTô nervoso";
+
+Console.WriteLine(frasememetomervoso);
